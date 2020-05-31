3b5cf4d77ecb7cde7f4cf67a7b4ec45325a11b41
Kahitar <Niklasarens1@gmx.de>
2020-05-31 13:45:51 +0200

    Refactor GUI to always show Options frame (even when starting a new game)

--- tft_scouter.py
+++ tft_scouter.py
@@ -2,95 +2,123 @@ import copy
 import tkinter as tk
 from tkinter import messagebox
 
-class CircularBuffer():
-	def __init__(self):
-		pass
-
 class GameFrame(tk.Frame):
-	def __init__(self, parent, game):
-		super().__init__(parent)
-		
-		self.game = game
-		self.parent = parent
+	def __init__(self, master, gameLogic):
+		super().__init__(master, bg="orange", height=225, width=280)
 
-		self.main_frame = tk.Frame(self, bg="orange", height=225, width=280)
-		self.main_frame.grid(row=0, column=0, sticky="nsew", padx=(5,0), pady=(5,0))
-		self.main_frame.grid_propagate(False)
-
-		self.options_frame = tk.Frame(self, bg="orange")
-		self.options_frame.grid(row=0, column=1, sticky="nsew", padx=(5,5), pady=(5,0))
-		self.options_frame.grid_rowconfigure(0,weight=0)
-		self.options_frame.grid_rowconfigure(1,weight=1)
-		self.options_frame.grid_rowconfigure(2,weight=0)
-		self.options_frame.grid_rowconfigure(3,weight=0)
-		tk.Label(self.options_frame, text="Options", bg="#99ff99", width=5).grid(row=0, column=0, sticky="new", padx=5, pady=5)
-		tk.Checkbutton(self.options_frame, text="Always on top", variable=parent.always_on_top).grid(row=1, column=0, sticky="new", padx=5, pady=0)
-		tk.Button(self.options_frame, text="Reset played", command=self.reset).grid(row=2, column=0, sticky="nsew", padx=5, pady=(5,0))
-		tk.Button(self.options_frame, text="Revive all", command=self.revive_all).grid(row=3, column=0, sticky="nsew", padx=5, pady=(5,0))
-		tk.Button(self.options_frame, text="Next Game", command=self.end_game, bg="#ff5555").grid(row=4, column=0, sticky="nsew", padx=5, pady=5)
+		self.gameLogic = gameLogic
 
 		self.player_buttons = dict()
 		self.delete_buttons = dict()
-		for i, (player_idx, player_name) in enumerate(self.game.players.items()):
-			self.player_buttons[player_idx] = tk.Button(self.main_frame, text=player_name, command=lambda player_idx=player_idx: self.played_player(player_idx))
-			self.delete_buttons[player_idx] = tk.Button(self.main_frame, text="X", bg="red", command=lambda player_idx=player_idx: self.delete_player(player_idx))
 
-		tk.Label(self.main_frame, text="Possible opponents", bg="#99ff99", width=18).grid(row=0, column=0, columnspan=2, sticky="nsew", padx=(5,0), pady=(5,5))
-		tk.Label(self.main_frame, text="Last played", bg="#99ff99", width=18).grid(row=0, column=2, columnspan=2, sticky="nsew", padx=(5,5), pady=(5,5))
+		self.reset()
 
-		self.update_info()
+	def reset(self):
+		self.forget_buttons()
+		
+		for i, (player_idx, player_name) in enumerate(self.gameLogic.players.items()):
+			self.player_buttons[player_idx] = tk.Button(self, text=player_name, command=lambda player_idx=player_idx: self.played_player(player_idx))
+			self.delete_buttons[player_idx] = tk.Button(self, text="X", bg="red", command=lambda player_idx=player_idx: self.delete_player(player_idx))
+
+		tk.Label(self, text="Possible opponents", bg="#99ff99", width=18).grid(row=0, column=0, columnspan=2, sticky="nsew", padx=(5,0), pady=(5,5))
+		tk.Label(self, text="Last played", bg="#99ff99", width=18).grid(row=0, column=2, columnspan=2, sticky="nsew", padx=(5,5), pady=(5,5))
+
+		self.update_played()
 
 	def played_player(self, player_idx):
-		self.game.played(player_idx)
+		self.gameLogic.played(player_idx)
 
-		self.update_info()
+		self.update_played()
 
 	def delete_player(self, player_idx):
-		self.game.player_died(player_idx)
+		self.gameLogic.player_died(player_idx)
 		self.player_buttons[player_idx].grid_forget()
 		self.delete_buttons[player_idx].grid_forget()
 		
-		self.update_info()
+		self.update_played()
 
-	def reset(self):
-		self.game.reset()
-		self.update_info()
+	def update_played(self):
+		self.forget_buttons()
 
-	def revive_all(self):
-		self.game.revive_all()
-		self.update_info()
+		for i, opponent in enumerate(self.gameLogic.get_possible_opponents()):
+			self.player_buttons[opponent].grid(row=i+1, column=0, sticky="nsew", padx=(10,0), pady=(0,1))
+			self.player_buttons[opponent]["state"] = tk.NORMAL
+			self.delete_buttons[opponent].grid(row=i+1, column=1)
 
-	def end_game(self):
-		if messagebox.askokcancel("Start new game?", " Are you sure you want to start a new game? \nAll the player names will be deleted."):
-			self.parent.next_stage()
+		for i, opponent in enumerate(self.gameLogic.get_played_opponents()):
+			self.player_buttons[opponent].grid(row=i+1, column=2, sticky="nsew", padx=(10,0), pady=(0,1))
+			self.player_buttons[opponent]["state"] = tk.DISABLED
+			self.delete_buttons[opponent].grid(row=i+1, column=3, padx=(5,0))
 
-	def update_info(self):
+	def forget_buttons(self):
 		for _, button in self.player_buttons.items():
 			button.grid_forget()
 
 		for _, button in self.delete_buttons.items():
 			button.grid_forget()
 
-		for i, opponent in enumerate(self.game.get_possible_opponents()):
-			self.player_buttons[opponent].grid(row=i+1, column=0, sticky="nsew", padx=(10,0), pady=(0,1))
-			self.player_buttons[opponent]["state"] = tk.NORMAL
-			self.delete_buttons[opponent].grid(row=i+1, column=1)
+class OptionsFrame(tk.Frame):
+	def __init__(self, parent, gameLogic, gameFrame):
+		super().__init__(parent, bg="orange")
+		
+		self.gameLogic = gameLogic
+		self.gameFrame = gameFrame
+		self.parent = parent
 
-		for i, opponent in enumerate(self.game.get_played_opponents()):
-			self.player_buttons[opponent].grid(row=i+1, column=2, sticky="nsew", padx=(10,0), pady=(0,1))
-			self.player_buttons[opponent]["state"] = tk.DISABLED
-			self.delete_buttons[opponent].grid(row=i+1, column=3, padx=(5,0))
+		self.grid_rowconfigure(0,weight=0)
+		self.grid_rowconfigure(1,weight=1)
+		self.grid_rowconfigure(2,weight=0)
+		self.grid_rowconfigure(3,weight=0)
+		tk.Label(self, text="Options", bg="#99ff99", width=5).grid(row=0, column=0, sticky="new", padx=5, pady=5)
+		tk.Checkbutton(self, text="Always on top", variable=parent.always_on_top).grid(row=1, column=0, sticky="new", padx=5, pady=0)
+		self.reset_button = tk.Button(self, text="Reset played", command=self.reset)
+		self.reset_button.grid(row=2, column=0, sticky="nsew", padx=5, pady=(5,0))
+		self.revive_button = tk.Button(self, text="Revive all", command=self.revive_all)
+		self.revive_button.grid(row=3, column=0, sticky="nsew", padx=5, pady=(5,0))
+		self.next_game_button = tk.Button(self, text="New Game", command=self.end_game, bg="#ff5555")
+		self.next_game_button.grid(row=4, column=0, sticky="nsew", padx=5, pady=5)
+
+	def disable_game_buttons(self):
+		self.reset_button["state"] = tk.DISABLED
+		self.revive_button["state"] = tk.DISABLED
+		self.next_game_button["state"] = tk.DISABLED
+
+	def enable_game_buttons(self):
+		self.reset_button["state"] = tk.NORMAL
+		self.revive_button["state"] = tk.NORMAL
+		self.next_game_button["state"] = tk.NORMAL
+
+	def reset(self):
+		self.gameLogic.reset()
+		self.gameFrame.update_played()
+
+	def revive_all(self):
+		self.gameLogic.revive_all()
+		self.gameFrame.update_played()
+
+	def end_game(self):
+		if messagebox.askokcancel("Start new game?", " Are you sure you want to start a new game? \nAll the player names will be deleted."):
+			self.parent.next_stage()
 
 class GameLogic:
-	def __init__(self, players):
+	# How many players are kept in the played buffer
+	#   depending on how many players are remaining.
+	PLAYED_LOGIC = { # remaining : played buffer
+		7: 4, 6: 4, 5: 3, 
+		4: 2, 3: 2, 2: 1
+	}
+
+	def __init__(self):
+		self.players = dict()
+		self.reset()
+
+	def new_game(self, players):
 		self.players = dict()
 		for i, player in enumerate(players):
 			self.players[i] = player
-
 		self.players_backup = copy.copy(self.players)
-
-		self.played_against = list()
-		self.played_start_idx = -1
+		
+		self.reset()
 
 	def reset(self):
 		self.played_against = list()
@@ -110,15 +138,8 @@ class GameLogic:
 			self.reset()
 			return
 
-		# How many players are kept in the played buffer
-		#   depending on how many players are remaining.
-		PLAYED_LOGIC = {
-			7: 4, 6: 4, 5: 3, 
-			4: 2, 3: 2, 2: 1
-		}
-
 		self.played_start_idx += 1
-		if self.played_start_idx > PLAYED_LOGIC[remaining_players]-1:
+		if self.played_start_idx > self.PLAYED_LOGIC[remaining_players]-1:
 			self.played_start_idx = 0
 		
 		if self.played_start_idx + 1 > len(self.played_against):
@@ -153,20 +174,24 @@ class PlayerSelectionFrame(tk.Frame):
 	def __init__(self, master, playerList):
 		super().__init__(master, bg="orange")
 
+		self.grid_columnconfigure(0, weight=0)
+		self.grid_columnconfigure(1, weight=1)
+		self.grid_columnconfigure(2, weight=0)
+
 		self.playerList = playerList
 		self.stringVars = list()
 
 		self.setup_frame()
 
 	def setup_frame(self):
-		tk.Label(self, text="Enter opponent names", bg="#99ff99").grid(row=0, column=0, columnspan=2, sticky="nsew", pady=(3,5))
+		tk.Label(self, text="Enter opponent names", bg="#99ff99").grid(row=0, column=0, columnspan=2, sticky="nsew", padx=(5,0), pady=(5,2))
 
 		for i in range(7):
-			tk.Label(self, text="Player {}:".format(i+1)).grid(row=i+1, column=0, padx=3, pady=2)
+			tk.Label(self, text="Player {}:".format(i+1)).grid(row=i+1, column=0, padx=5, pady=3, sticky="w")
 			self.stringVars.append(tk.StringVar(self))
-			tk.Entry(self, textvariable=self.stringVars[-1]).grid(row=i+1, column=1, pady=2)
+			tk.Entry(self, textvariable=self.stringVars[-1]).grid(row=i+1, column=1, pady=3, sticky="nsew")
 
-		tk.Button(self, text="Start Game", command=self.start_game).grid(row=8, column=1)
+		tk.Button(self, text="Start Game", command=self.start_game).grid(row=i+1, column=2, padx=5, sticky="e")
 
 	def start_game(self):
 		for i in range(7):
@@ -181,14 +206,19 @@ class Game(tk.Tk):
 
 		self.grid_columnconfigure(0, weight=1)
 		self.grid_rowconfigure(0, weight=1)
-		self.grid_rowconfigure(1, weight=0)
 
 		self.minsize(408, 235)
 		self.title("TFT Scouter Assistent")
 
 		self.always_on_top = tk.BooleanVar()
-		self.always_on_top.set(False)
+		self.always_on_top.set(True)
 		self.always_on_top.trace_add('write', lambda x, y, z: self.set_always_on_top(self.always_on_top.get()))
+		self.set_always_on_top(self.always_on_top.get())
+
+		self.gameLogic = GameLogic()
+		self.gameFrame = GameFrame(self, self.gameLogic)
+		self.options_frame = OptionsFrame(self, self.gameLogic, self.gameFrame)
+		self.options_frame.grid(row=0, column=1, sticky="nsew", padx=(5,5), pady=5)
 
 		self.stage = 0
 		self.next_stage()
@@ -204,13 +234,19 @@ class Game(tk.Tk):
 
 	def start_player_selection_frame(self):
 		self.players = list()
-		playerSelection = PlayerSelectionFrame(self, self.players)
-		playerSelection.grid(row=0, column=0, sticky="nsew")
+		self.playerSelection = PlayerSelectionFrame(self, self.players)
+		self.playerSelection.grid(row=0, column=0, sticky="nsew", padx=(5,0), pady=5)
+
+		self.options_frame.disable_game_buttons()
 
 	def start_main_game(self):
-		gameLogic = GameLogic(self.players)
-		gameFrame = GameFrame(self, gameLogic)
-		gameFrame.grid(row=0, column=0, sticky="nsew")
+		self.playerSelection.grid_forget()
+		self.gameLogic.new_game(self.players)
+		self.gameFrame.reset()
+		self.gameFrame.grid(row=0, column=0, sticky="nsew", padx=(5,0), pady=5)
+		self.gameFrame.grid_propagate(False)
+
+		self.options_frame.enable_game_buttons()
 
 	def set_always_on_top(self, is_always_on_top):
 		self.wm_attributes("-topmost", is_always_on_top)
